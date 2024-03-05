898c297729cc2132b3e33f2afb06be68cce9ac4e
irvanshandika <shandikamuhammadirvan@gmail>
2024-03-05 21:52:10 +0700

    Add hero section, making not found page, and fix error can't load color

---
+++ src/data/dataCarousel.ts
@@ -0,0 +1,22 @@
+export const dataCarousel = [
+  {
+    imageUrl: "https://down-id.img.susercontent.com/file/id-11134207-7r98y-lmr9pwcelg6cb3",
+    alt: "Parfum",
+  },
+  {
+    imageUrl: "https://down-id.img.susercontent.com/file/id-11134207-7r98v-ln5mna8hktljaa",
+    alt: "Baju",
+  },
+  {
+    imageUrl: "https://down-id.img.susercontent.com/file/id-11134207-7r98w-lmisz5afs8x041",
+    alt: "Tas",
+  },
+  {
+    imageUrl: "https://down-id.img.susercontent.com/file/id-11134207-7r98y-lnojnt9um77l33",
+    alt: "Celana Joger",
+  },
+  {
+    imageUrl: "https://down-id.img.susercontent.com/file/5869b045eef9521023b59ea22289aa1f",
+    alt: "Hair Treatment",
+  },
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       fontFamily: {
         google: ["Google Sans", "sans-serif"],
-        roboto: ["Roboto", "sans-serif"],
+        roboto: ["Roboto Sans", "sans-serif"],
       },
     },
   },
