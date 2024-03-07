75e8ca13ae55b2508fc0467912190cdbd23f5a1d
irvanshandika <shandikamuhammadirvan@gmail>
2024-03-07 18:43:29 +0700

    Fix performance image5

--- src/data/ProductList.ts
+++ src/data/ProductList.ts
@@ -19,28 +19,28 @@ export const productList = [
 
 export const productCategories = [
   {
-    image: "https://maybeauty-skin.pages.dev/https://res.cloudinary.com/dszhlpm81/image/upload/v1709736323/assets/phKFNpB7tMHUeEMuMCiMoTyH4rJTs3vp/parfum_iye3yf.jpg/quality=auto",
+    image: "https://res.cloudinary.com/dszhlpm81/image/upload/v1709736323/assets/phKFNpB7tMHUeEMuMCiMoTyH4rJTs3vp/parfum_iye3yf.jpg",
     name: "Parfum",
     alt: "Gambar Parfume",
     category: "Parfum",
     url: "parfum",
   },
   {
-    image: "https://maybeauty-skin.pages.dev/https://res.cloudinary.com/dszhlpm81/image/upload/v1709736317/assets/phKFNpB7tMHUeEMuMCiMoTyH4rJTs3vp/celana_t3fjxq.jpg/quality=auto",
+    image: "https://res.cloudinary.com/dszhlpm81/image/upload/v1709736317/assets/phKFNpB7tMHUeEMuMCiMoTyH4rJTs3vp/celana_t3fjxq.jpg",
     name: "Pakaian",
     alt: "Gambar Pakaian",
     category: "Pakaian",
     url: "pakaian",
   },
   {
-    image: "https://maybeauty-skin.pages.dev/https://res.cloudinary.com/dszhlpm81/image/upload/v1709736320/assets/phKFNpB7tMHUeEMuMCiMoTyH4rJTs3vp/tas_mtssnc.jpg/quality=auto",
+    image: "https://res.cloudinary.com/dszhlpm81/image/upload/v1709736320/assets/phKFNpB7tMHUeEMuMCiMoTyH4rJTs3vp/tas_mtssnc.jpg",
     name: "Tas",
     alt: "Gambar Tas",
     category: "Tas",
     url: "tas",
   },
   {
-    image: "https://maybeauty-skin.pages.dev/https://res.cloudinary.com/dszhlpm81/image/upload/v1709736322/assets/phKFNpB7tMHUeEMuMCiMoTyH4rJTs3vp/perawatan_awskfl.jpg/quality=auto",
+    image: "https://res.cloudinary.com/dszhlpm81/image/upload/v1709736322/assets/phKFNpB7tMHUeEMuMCiMoTyH4rJTs3vp/perawatan_awskfl.jpg",
     name: "Perawatan & Kecantikan",
     alt: "Gambar Perawatan & Kecantikan",
     category: "Perawatan & Kecantikan",
