8d02df624241333bb202d2aacb73bdbe39621172
irvanshandika <shandikamuhammadirvan@gmail>
2024-03-17 14:38:11 +0700

    Add issue tracker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import sentry from "@sentry/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [
+    tailwind(), 
+    react(), 
+    sentry({
+      dsn: "https://f60b0da08f5b306dfcac62db58462a59@o4506924345393152.ingest.us.sentry.io/4506925173112832",
+      sourceMapsUploadOptions: {
+        project: "e-commerce",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+  ]
 });
