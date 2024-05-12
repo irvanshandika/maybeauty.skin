4f5acdae5fccc7f8b8bd82edfce600960f974977
irvanshandika <shandikamuhammadirvan@gmail>
2024-05-12 21:53:41 +0700

    Add team page

---
+++ src/data/Team.ts
@@ -0,0 +1,32 @@
+export const ListTeam = [
+  {
+    nama: "Muhammad Irvan Shandika",
+    nim: "21.11.4285",
+    image: "https://res.cloudinary.com/dszhlpm81/image/upload/v1715524845/assets/UFPMOvuUtfuMxUpG2yy%2BnF743hBOeLEzqjNEDiHixcQ%3D/undraw_drink_coffee_v3au_htfepd.svg",
+    kelas: "21IF07",
+  },
+  {
+    nama: "Faiq Hidayat Dzakwan",
+    nim: "21.11.4274",
+    image: "https://res.cloudinary.com/dszhlpm81/image/upload/v1715524845/assets/UFPMOvuUtfuMxUpG2yy%2BnF743hBOeLEzqjNEDiHixcQ%3D/undraw_drink_coffee_v3au_htfepd.svg",
+    kelas: "21IF07",
+  },
+  {
+    nama: "Laili Aulia Fitri",
+    nim: "21.11.4329",
+    image: "https://res.cloudinary.com/dszhlpm81/image/upload/v1715524845/assets/UFPMOvuUtfuMxUpG2yy%2BnF743hBOeLEzqjNEDiHixcQ%3D/undraw_coffee_time_e8cw_o1nzyb.svg",
+    kelas: "21IF07",
+  },
+  {
+    nama: "Nahdah Sakinatussyifa",
+    nim: "21.11.4261",
+    image: "https://res.cloudinary.com/dszhlpm81/image/upload/v1715524845/assets/UFPMOvuUtfuMxUpG2yy%2BnF743hBOeLEzqjNEDiHixcQ%3D/undraw_coffee_time_e8cw_o1nzyb.svg",
+    kelas: "21IF07",
+  },
+  {
+    nama: "Gusti Ayu Putu Febriyanti",
+    nim: "21.11.4326",
+    image: "https://res.cloudinary.com/dszhlpm81/image/upload/v1715524845/assets/UFPMOvuUtfuMxUpG2yy%2BnF743hBOeLEzqjNEDiHixcQ%3D/undraw_coffee_time_e8cw_o1nzyb.svg",
+    kelas: "21IF07",
+  },
+];
