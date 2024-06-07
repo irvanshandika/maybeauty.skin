5ab7f5bb45971981563e9c431de886ef8e52f2a1
irvanshandika <shandikamuhammadirvan@gmail>
2024-06-07 22:02:34 +0700

    Add sponsor section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", "./node_modules/flowbite/**/*.js"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", "./node_modules/flowbite/**/*.js", './node_modules/preline/preline.js',],
   theme: {
     extend: {
       fontFamily: {
@@ -9,5 +9,5 @@ export default {
       },
     },
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [require("flowbite/plugin"), require('preline/plugin'),],
 };
